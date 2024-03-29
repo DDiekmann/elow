0cf04ee9366c68b6b15af3704824a02b93f7e192
DD <d.d@d.com>
2024-03-29 15:17:44 +0100

    Nix Develop

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  description = "An simple elow calculator for google sheets";
+
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    nix-cde.url = "github:takeda/nix-cde";
+  };
+
+  outputs = { self, flake-utils, nix-cde, ... }: flake-utils.lib.eachDefaultSystem (build_system:
+  let
+    cde = is_shell: nix-cde.lib.mkCDE ./project.nix { inherit build_system is_shell self; };
+    cde-docker = nix-cde.lib.mkCDE ./project.nix {
+      inherit build_system self;
+      host_system = "x86_64-linux";
+    };
+  in {
+    packages.default = (cde false).outputs.out_python;
+    packages.docker = cde-docker.outputs.out_docker;
+    devShells.default = (cde true).outputs.out_shell;
+  });
+}
---
+++ project.nix
@@ -0,0 +1,47 @@
+{ config, modulesPath, nix2container, pkgs, ... }:
+
+{
+  require = [
+    "${modulesPath}/languages/python-poetry.nix"
+    "${modulesPath}/builders/docker-nix2container.nix"
+    "${modulesPath}/tools/mod-lean-python.nix"
+  ];
+
+  name = "elow"; # Replace with your project name
+  src = ./elow; # Replace with your project's source directory
+
+  # files to exclude (there often are files that you need to have in git, but
+  # you don't want nix to rebuild your app if they change)
+  # simpliarly to .gitignore you can also exclude everything and implicitly
+  # list files you want included
+  src_exclude = [''
+    .vscode
+    README.md
+  ''];
+
+  lean_python = {
+    enable = true;
+    package = pkgs.python311;
+    expat = true;
+    zlib = true;
+  };
+
+  python = {
+    enable = true;
+    package = config.out_lean_python;
+    inject_app_env = true;
+    prefer_wheels = false;
+  };
+
+  docker = {
+    enable = true;
+    command = [ "${config.out_python}/bin/hello" ]; # Replace with your project's entrypoint
+    layers = with nix2container; [
+      (buildLayer { deps = [ config.out_lean_python ]; })
+    ];
+  };
+
+  dev_commands = with pkgs; [
+    dive
+  ];
+}
